8cf6e3d8dd2524560126f91604d6e963d98f22bb
BugenZhao <i@bugenzhao.com>
2020-04-17 17:39:35 +0800

    Problem 1

--- LeetCode.playground/Pages/1-TwoSum.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/1-TwoSum.xcplaygroundpage/Contents.swift
@@ -1,1 +1,15 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict: [Int: Int] = [:]
+        for (idx, num) in nums.enumerated() {
+            if let idxo = dict[target - num] {
+                return [idxo, idx]
+            } else {
+                dict[num] = idx
+            }
+        }
+        return [233, 666]
+    }
+}
 
+assert(Solution().twoSum([2, 7, 11, 15], 9) == [0, 1])
