2a95298fe2c0d37051c5e91be134d1f2ceff0368
BugenZhao <i@bugenzhao.com>
2020-04-17 22:45:39 +0800

    Added LinkedList

--- LeetCode.playground/Pages/Hello.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/Hello.xcplaygroundpage/Contents.swift
@@ -1,6 +1,14 @@
 import Cocoa
 
 var array = ["Swift", "playground", "LeetCode", "Bugen! 😄"]
+array.forEach { print("Hello, \($0)") }
 
-array.forEach{ print("Hello, \($0)") }
+
+/// Test for linked list
+var node = ListNode.makeLinkedList(from: [1, 0, 1, 8])
+while node != nil {
+    print(node!.val, terminator: "")
+    node = node!.next
+}
+print()
 
---
+++ LeetCode.playground/Sources/LinkedList.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+/// Bugen's extension to linked list
+public extension ListNode {
+    static func makeLinkedList <T:Collection> (from collection: T) -> ListNode?
+    where T.Element == Int
+    {
+        var head: ListNode? = nil
+        var p: ListNode? = nil
+        for element in collection {
+            if head == nil {
+                head = ListNode(element)
+                p = head
+            } else {
+                p!.next = ListNode(element)
+                p = p!.next
+            }
+        }
+
+        return head
+    }
+}
