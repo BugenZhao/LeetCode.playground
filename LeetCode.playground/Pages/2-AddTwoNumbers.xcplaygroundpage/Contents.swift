ed55a7544545250e8c787c866e588320818d939c
BugenZhao <i@bugenzhao.com>
2020-04-18 00:35:27 +0800

    Problem 2

---
+++ LeetCode.playground/Pages/2-AddTwoNumbers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+class Solution {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?, _ carried: Bool = false) -> ListNode? {
+        guard let l1 = l1 else {
+            if carried {
+                return addTwoNumbers(ListNode(1), l2)
+            } else {
+                return l2
+            }
+        }
+        guard let l2 = l2 else {
+            if carried {
+                return addTwoNumbers(l1, ListNode(1))
+            } else {
+                return l1
+            }
+        }
+        let sum = l1.val + l2.val + (carried ? 1 : 0)
+        let toCarry = sum > 9
+        let result = ListNode(sum % 10)
+        result.next = addTwoNumbers(l1.next, l2.next, toCarry)
+
+        return result
+    }
+}
+
+
+let list1 = ListNode.makeLinkedList(from: [2, 4, 3])
+let list2 = ListNode.makeLinkedList(from: [5, 6, 4])
+testEqual(Solution().addTwoNumbers(list1, list2),
+    ListNode.makeLinkedList(from: [7, 0, 8]))
--- LeetCode.playground/Pages/Scratch.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/Scratch.xcplaygroundpage/Contents.swift
@@ -16,8 +16,24 @@ extension ListNode {
     }
 }
 
+extension ListNode: Equatable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        return lhs.toArray() == rhs.toArray()
+    }
+}
+
 let list = ListNode.makeLinkedList(from: [1, 2, 3, 4, 2, 1])
 list?.toArray()
 list?.next?.toArray()
+
+list?.next?.next == ListNode.makeLinkedList(from: [3, 4, 2, 1])
+
+extension ListNode: CustomStringConvertible {
+    public var description: String {
+        return String(describing: self.toArray())
+    }
+}
+
+list
 
 //: [Next](@next)
