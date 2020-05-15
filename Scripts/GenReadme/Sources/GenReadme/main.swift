54605d99849b47657eb7116587f2d1301c180189
BugenZhao <i@bugenzhao.com>
2020-05-15 22:40:10 +0800

    Updated: git operations for GenReadme

--- Scripts/GenReadme/Sources/GenReadme/main.swift
+++ Scripts/GenReadme/Sources/GenReadme/main.swift
@@ -1,3 +1,5 @@
+import ShellOut
+
 var dict = Question.getRemoteQuestions()
 Question.getLocalInfo(dict: &dict)
 
@@ -5,4 +7,14 @@ Writer.writeReadme(dict, to: readmeURL)
 Writer.writeXcodePage(dict, to: helloPageURL)
 Tag.allCases.forEach { Writer.writeTag(dict, tag: $0, to: makeURL(tag: $0)) }
 
-print("Done".green)
+print(try shellOut(to: "git", arguments: ["status"]))
+print("Commit message: ".yellow, terminator: "")
+if let message = readLine() {
+    try shellOut(to: "git", arguments: ["add", "-A"])
+    try shellOut(to: .gitCommit(message: message))
+    try shellOut(to: .gitPush())
+    print("Done".green)
+} else {
+    print("Done without git operations".green)
+}
+
